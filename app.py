cda7873ed80157a19ecd98d9b2d662439d31f2fe
JuliaRoughley <roughleyjulia@gmail.com>
2023-08-12 17:12:38 +0100

    Loading Authors into select list on add_book UI

--- app.py
+++ app.py
@@ -41,6 +41,34 @@ def add_author_post():
     return render_template("add_author.html", message="Author added!")
 
 
+@app.route("/add_book", methods=["GET"])
+def add_book_get():
+    try:
+        authors = data_models.Author.query.all()
+        return render_template("add_book.html", authors=authors)
+    except Exception as ex:
+        return render_template("add_book.html", message="Cannot load authors at this time!")
+
+
+
+@app.route("/add_book", methods=["POST"])
+def add_book_post():
+
+    name = request.form['name']
+    birthdate = datetime.date.fromisoformat(request.form['birthdate'])
+    date_of_death = datetime.date.fromisoformat(request.form['date_of_death'])
+
+    try:
+        new_author = data_models.Author(
+            name=name, birth_date=birthdate, date_of_death=date_of_death)
+        data_models.db.session.add(new_author)
+        data_models.db.session.commit()
+    except Exception as ex:
+        return render_template("add_author.html", message="Can't add this Author!")
+
+    return render_template("add_author.html", message="Author added!")
+
+
 if __name__ == "__main__":
     # with app.app_context():
     #     db.create_all()
